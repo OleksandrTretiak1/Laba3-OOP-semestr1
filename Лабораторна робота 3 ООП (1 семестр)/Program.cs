9b95d87fc3208ca0ff5d534a1f7ac754487b132b
OleksandrTretiak1 <ssanka467@gmail.com>
2024-11-28 21:19:01 +0200

    Добавьте файлы проекта.

---
+++ Лабораторна робота 3 ООП (1 семестр)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_робота_3_ООП__1_семестр_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
